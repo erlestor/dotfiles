02850baac5d329aed2aece04c356ef0aa9be37fe
Erlend Storsve <erlendstorsve@gmail.com>
2025-10-19 02:54:54 +0200

    sync

--- powershell/profile.ps1
+++ powershell/profile.ps1
@@ -1,7 +1,6 @@
 # Aliases
 Set-Alias -Name "p" -Value "pnpm"
 Set-Alias -Name "lg" -Value "lazygit"
-Set-Alias docker-start "C:/Program Files/Docker/Docker/Docker Desktop.exe"
 
 # shortcut functions
 function e {
@@ -18,6 +17,7 @@ function wssh {
 }
 
 # KOMOREBI
+# I don't know if this is working. Or the system variables
 $Env:KOMOREBI_CONFIG_HOME = "C:\Users\erlen\.config\komorebi"
 $Env:KOMOREBI_AHK_EXE = "C:\Program Files\AutoHotkey\v2\AutoHotkey64.exe"
 
@@ -33,12 +33,13 @@ if (Test-Path($ChocolateyProfile)) {
 # OH MY POSH PROMPT
 oh-my-posh init pwsh --config "$home/.config/amro.omp.json" | Invoke-Expression
 
-# IDK
+# Vi mode
 Set-PSReadLineOption -EditMode Vi
+# Set-PSReadLineOption -ViModeIndicator Cursor
+
+# Autocomplete
 Set-PSReadLineOption -PredictionSource HistoryAndPlugin
 Set-PSReadLineOption -PredictionViewStyle Inline
-
-# OTHER HOTKEYS
 Set-PSReadLineKeyHandler -Chord Ctrl+u -Function PreviousHistory
 Set-PSReadLineKeyHandler -Chord Ctrl+i -Function NextHistory
 Set-PSReadLineKeyHandler -Key Tab -Function AcceptSuggestion
@@ -48,5 +49,4 @@ $env:EDITOR = "nvim"
 
 # ZOXIDE
 Invoke-Expression (& { (zoxide init powershell | Out-String) })
-
 Set-Alias cd z -Option AllScope
